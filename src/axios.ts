7fcbdb8f6f8e5724b7c1c328e8fc8a93b86972cb
Tóth Kevin <tothkevin0716@gmail.com>
2024-03-21 09:48:55 +0100

    Added headers to axios

--- src/axios.ts
+++ src/axios.ts
@@ -2,4 +2,7 @@ import axios from "axios";
 
 export default axios.create({
     baseURL: "http://localhost:8080/",
+    headers: {
+        Authorization: 'Bearer ' + localStorage.getItem('accessToken')
+    }
 });
