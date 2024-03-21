5e27d455aaa1ed32cfdd43ed8451a2f035e1471b
Tóth Kevin <tothkevin0716@gmail.com>
2024-03-21 09:10:28 +0100

    Added logic to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,19 @@
+import {useContext} from "react";
+import {AuthContext} from "../contexts/AuthContext";
+
 function Navbar() {
+    const {token} = useContext(AuthContext);
     return (
         <nav>
-            <ul>
-                <li><a href="/homepage">Homepage</a></li>
-                <li><a href="/notifications">Notifications</a></li>
-                <li><a href="/tradeoffers">Trade offers</a></li>
-                <li><a href="/itemcreation">Item Creation</a></li>
-                <li><a href="/profile">Profile</a></li>
-            </ul>
+            {!token ? null :
+                <ul>
+                    <li><a href="/homepage">Homepage</a></li>
+                    <li><a href="/notifications">Notifications</a></li>
+                    <li><a href="/tradeoffers">Trade offers</a></li>
+                    <li><a href="/itemcreation">Item Creation</a></li>
+                    <li><a href="/profile">Profile</a></li>
+                </ul>
+            }
         </nav>
     );
 }
