a78b6ab77f6faa169160ae341b45a345e8dc2116
Tóth Kevin <tothkevin0716@gmail.com>
2024-04-05 09:50:27 +0200

    Created Category enum inside constants directory

---
+++ src/constants/category.ts
@@ -0,0 +1,14 @@
+enum Category {
+    VEHICLE = 'Vehicle',
+    HOME = 'Home',
+    HOUSEHOLD = 'Household',
+    ELECTRONICS = 'Electronics',
+    FREETIME = 'Free time',
+    SPORT = 'Sport',
+    FASHION = 'Fashion',
+    COLLECTIBLES = 'Collectibles',
+    PETS = 'Pets',
+    OTHER = 'Other'
+}
+
+export default Category;
