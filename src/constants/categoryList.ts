32c20ee2ed0aa914ab10b97fc88c3dd4add97f86
Tóth Kevin <tothkevin0716@gmail.com>
2024-04-05 10:25:07 +0200

    Created categoryList inside constants directory

---
+++ src/constants/categoryList.ts
@@ -0,0 +1,14 @@
+const categoryList = [
+    'Vehicle',
+    'Home',
+    'Household',
+    'Electronics',
+    'Free time',
+    'Sport',
+    'Fashion',
+    'Collectibles',
+    'Pets',
+    'Other'
+];
+
+export default categoryList;
