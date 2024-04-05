154e4e4a70a0322ffb0ad7e5b43166919bf8ee36
Tóth Kevin <tothkevin0716@gmail.com>
2024-04-05 09:28:13 +0200

    Created categoryList common array

---
+++ src/categoryList.ts
@@ -0,0 +1,14 @@
+const categoryList = [
+    'VEHICLE',
+    'HOME',
+    'HOUSEHOLD',
+    'ELECTRONICS',
+    'FREETIME',
+    'SPORT',
+    'FASHION',
+    'COLLECTIBLES',
+    'PETS',
+    'OTHER'
+];
+
+export default categoryList;
