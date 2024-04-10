f5911fffef5011fac660be1dffcbe6cd92cf5806
Tóth Kevin <tothkevin0716@gmail.com>
2024-04-10 11:19:10 +0200

    Edited ProfilePicture component

--- src/components/ProfilePicture.tsx
+++ src/components/ProfilePicture.tsx
@@ -1,5 +1,6 @@
 import default_profile_pic from "../assets/default_profile_pic.png";
 import React from "react";
+import {FiUpload} from "react-icons/fi";
 
 interface ProfilePictureProps {
     profilePicture: string,
@@ -15,11 +16,7 @@ function ProfilePicture(props: ProfilePictureProps) {
                      alt="Profile picture"/>
                 <label
                     className="w-64 h-64 group hover:bg-gray-200 opacity-60 rounded-full absolute flex justify-center items-center cursor-pointer transition duration-500">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                         stroke="currentColor" className="hidden group-hover:block w-12 h-12">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                              d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5"/>
-                    </svg>
+                    <FiUpload className="hidden group-hover:block w-12 h-12"/>
                     <input
                         style={{display: 'none'}}
                         type="file"
