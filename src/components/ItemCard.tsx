25e451634fdba9888b28f2340cca20282c41f366
Tóth Kevin <tothkevin0716@gmail.com>
2024-04-10 14:35:15 +0200

    Added props to ItemCard component

--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -1,7 +1,14 @@
 import {useState} from "react";
 import ItemCardData from "../interfaces/itemCardData";
+import ItemCardWithIdData from "../interfaces/itemCardWithIdData";
 
-function ItemCard(item: ItemCardData) {
+interface ItemCardProps {
+    item: ItemCardData | ItemCardWithIdData,
+    buttonText: string,
+    onButtonClick?: () => void,
+}
+
+function ItemCard(props: ItemCardProps) {
     const [isFlipped, setIsFlipped] = useState(false);
 
     function handleFlip() {
@@ -12,15 +19,21 @@ function ItemCard(item: ItemCardData) {
             <div className="group h-96 w-80 [perspective:1000px]" onClick={handleFlip}>
                 <div className={`relative h-full w-full rounded-xl shadow-xl transition-all duration-500 [transform-style:preserve-3d] ${isFlipped && "[transform:rotateY(180deg)]"}`}>
                     <div className="absolute rounded overflow-hidden shadow-lg max-w-sm">
-                        <img className="w-full" src={item.itemPicture} alt="card picture"/>
+                        <img className="w-full"
+                             src={props.item.itemPicture}
+                             alt="card picture"/>
                         <div className="px-6 py-4">
-                            <div className="font-bold text-xl mb-2">{item.title}</div>
+                            <div className="font-bold text-xl mb-2">{props.item.title}</div>
                         </div>
                     </div>
                     <div className="absolute h-full w-full rounded bg-white px-12 [transform:rotateY(180deg)] [backface-visibility:hidden]">
                         <div className="flex min-h-full flex-col ">
-                            <p className="text-base">{item.description}</p>
-                            <button className="mt-2 rounded-md bg-neutral-800 py-1 px-2 text-sm hover:bg-neutral-900">View</button>
+                            <p className="text-base">{props.item.description}</p>
+                            <button
+                                className="mt-2 rounded-md bg-neutral-800 py-1 px-2 text-sm hover:bg-neutral-900"
+                                onClick={props.onButtonClick}>
+                                    {props.buttonText}
+                            </button>
                         </div>
                     </div>
                 </div>
