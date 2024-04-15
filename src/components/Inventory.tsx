76cc3b780d0f5ceb99d96367da9c7502f595860c
Tóth Kevin <tothkevin0716@gmail.com>
2024-04-15 13:52:59 +0200

    Added content to Inventory component

--- src/components/Inventory.tsx
+++ src/components/Inventory.tsx
@@ -1,6 +1,42 @@
+import {useContext, useEffect, useState} from "react";
+import {HttpContext} from "../providers/HttpProvider";
+import {Grid} from "@mui/material";
+import ItemCard from "./ItemCard";
+import ItemCardWithIdData from "../interfaces/itemCardWithIdData";
+import {useNavigate} from "react-router-dom";
+
 function Inventory() {
+    const axios = useContext(HttpContext);
+
+    const [itemList, setItemList] = useState<ItemCardWithIdData[]>([]);
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        axios.get('user/items')
+            .then((response) => {
+                setItemList(response.data);
+            });
+    }, [axios]);
+
+    function handleButtonClick(selectedItem: ItemCardWithIdData) {
+        navigate('/updateitem/' + selectedItem.id);
+    }
+
     return(
-        <h1>Inventory</h1>
+        <Grid container columns={{ xl: 15, lg: 12, md: 12, xs: 12 }}>
+            {itemList
+                .sort((itemA, itemB) => itemB.id - itemA.id)
+                .map((item) => (
+                <Grid key={item.id} item xl={3} lg={3} md={4} xs={6}>
+                    <ItemCard
+                        item={{...item, itemPicture: `data:image/png;base64, ${item.itemPicture}`}}
+                        onButtonClick={() => handleButtonClick(item)}
+                        buttonText="Edit"
+                    />
+                </Grid>
+            ))}
+        </Grid>
     );
 }
 
