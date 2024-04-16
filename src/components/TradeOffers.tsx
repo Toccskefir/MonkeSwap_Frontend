814692d4e36f070cce92ea768d74fff0e8adca1e
Tóth Kevin <tothkevin0716@gmail.com>
2024-04-16 09:23:37 +0200

    Added content to TradeOffers component

--- src/components/TradeOffers.tsx
+++ src/components/TradeOffers.tsx
@@ -1,5 +1,107 @@
+import {useContext, useEffect, useState} from "react";
+import {HttpContext} from "../providers/HttpProvider";
+import TradeOfferData from "../interfaces/tradeOfferData";
+import TradeOffer from "./TradeOffer";
+
 function TradeOffers() {
-    return (<h1>trade offers</h1>);
+    const axios = useContext(HttpContext);
+
+    const [incomingOffers, setIncomingOffers] = useState<TradeOfferData[]>([]);
+    const [offeredOffers, setOfferedOffers] = useState<TradeOfferData[]>([]);
+    const [type, setType] = useState<'INCOMING'|'OFFERED'>('INCOMING');
+    const [errorMessage, setErrorMessage] = useState('Your incoming offers will be displayed here');
+
+    useEffect(() => {
+        getIncoming();
+    }, [axios]);
+
+    function getIncoming() {
+        axios.get('tradeoffer/incoming')
+            .then((response) => {
+                setIncomingOffers(response.data);
+                setErrorMessage('Your incoming offers will be displayed here');
+            }).catch((error) => {
+            if (error.response) {
+                setErrorMessage(error.response.data);
+            }
+        });
+    }
+
+    function getOffered() {
+        axios.get('tradeoffer/offered')
+            .then((response) => {
+                setOfferedOffers(response.data);
+                setErrorMessage('Your outgoing offers will be displayed here');
+            }).catch((error) => {
+                if (error.response) {
+                    setErrorMessage(error.response.data);
+                }
+        });
+    }
+
+    function handleTypeChangeIncoming() {
+        setType('INCOMING');
+        getIncoming();
+    }
+
+    function handleTypeChangeOffered() {
+        setType('OFFERED');
+        getOffered();
+    }
+
+    if (type === 'INCOMING') {
+        return (
+            <>
+                <button onClick={handleTypeChangeIncoming}>Incoming offers</button>
+                <button onClick={handleTypeChangeOffered}>Outgoing offers</button>
+
+                {incomingOffers.length === 0 ?
+                    <div className="text-4xl text-center text-black m-5">
+                        {errorMessage}
+                    </div> :
+                    <div>
+                        {incomingOffers!.map((tradeOffer) => (
+                            <TradeOffer
+                                key={tradeOffer.id}
+                                tradeOffer={tradeOffer}
+                                type={type}
+                                accept={() => {
+                                }}
+                                decline={() => {
+                                }}
+                            />
+                        ))}
+                    </div>
+                }
+
+            </>
+        );
+    } else {
+        return (
+            <>
+                <button onClick={handleTypeChangeIncoming}>Incoming offers</button>
+                <button onClick={handleTypeChangeOffered}>Outgoing offers</button>
+
+                { offeredOffers.length === 0 ?
+                    <div className="text-4xl text-center text-black m-5">
+                        {errorMessage}
+                    </div> :
+                    <div>
+                        {offeredOffers!.map((tradeOffer) => (
+                            <TradeOffer
+                                key={tradeOffer.id}
+                                tradeOffer={tradeOffer}
+                                type={type}
+                                accept={() => {}}
+                                decline={() => {}}
+                            />
+                        ))}
+                    </div>
+                }
+            </>
+        );
+    }
+
 }
 
 export default TradeOffers;
